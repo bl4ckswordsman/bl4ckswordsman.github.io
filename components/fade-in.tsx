fc77fe098f6c7871b1965b3b59b38aa7369d396d
amarildo <amarildo.rajta@gmail.com>
2024-01-28 15:24:52 +0100

    test deployment

--- components/fade-in.tsx
+++ components/fade-in.tsx
@@ -1,19 +1,24 @@
-import { motion, useAnimation } from 'framer-motion';
-import { useEffect } from 'react';
+import {motion, useAnimation} from 'framer-motion';
+import {ReactNode, useEffect} from 'react';
 
-export const FadeIn = ({ children }) => {
+interface FadeInProps {
+    children: ReactNode;
+}
+
+export const FadeIn = ({children}: FadeInProps) => {
     const controls = useAnimation();
 
     useEffect(() => {
         controls.start({
             opacity: 1,
             transition: {duration: 1}
-        }).then(r =>{});
+        }).then(r => {
+        });
     }, [controls]);
 
     return (
         <motion.div
-            initial={{ opacity: 0 }}
+            initial={{opacity: 0}}
             animate={controls}
         >
             {children}
