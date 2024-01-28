70f7e230121c2c8a8c6d856ad2eb0d0c114172c8
amarildo <amarildo.rajta@gmail.com>
2024-01-28 14:43:59 +0100

    test deployment

--- components/particle-orb-background.tsx
+++ components/particle-orb-background.tsx
@@ -1,8 +1,9 @@
 import '@/app/styles/ParticleOrbBg.css';
+
 export const ParticleOrbBackground = () => {
     return (
         <div className="wrap">
-            {Array.from({ length: 300 }, (_, i) => (
+            {Array.from({length: 300}, (_, i) => (
                 <div key={i} className="c"></div>
             ))}
         </div>
