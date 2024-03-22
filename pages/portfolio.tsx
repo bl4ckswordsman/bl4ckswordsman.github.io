c5e76735cbfae715fd3d1f513087cba4852aa289
amarildo <amarildo.rajta@gmail.com>
2024-03-22 19:06:34 +0100

    refactor and optimize a lot of code and add a loading skeleton for the portfolio page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,141 +1,147 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from "@/components/page-header";
 import RootLayout from "@/app/layout";
 import {Card} from "@/components/ui/card";
 import {Accordion, AccordionItem, AccordionTrigger, AccordionContent} from "@/components/ui/accordion";
 import {Badge} from "@/components/ui/badge";
-import {Spacer, Card as CardNextUI, CardBody, CardHeader} from "@nextui-org/react";
+import {
+    Spacer,
+    Card as CardNextUI,
+    CardBody,
+    CardHeader as CardHeaderNextUI
+} from "@nextui-org/react";
 import {CopyEmailButton, SendEmailButton} from "@/components/email-buttons";
 import {Divider} from "@nextui-org/divider";
+import {FadeIn} from "@/components/fade-in";
+import {LoadingSkeleton} from "@/components/loading-skeleton";
+
+/*// Encryption example
+const encrypted = encrypt('Hello World!');
+
+console.log('Encrypted:', encrypted);
+
+const decrypted = decrypt(encrypted);
+console.log('Decrypted:', decrypted);*/
+
+const renderAccordionItems = (data: any, keys: string[]) => {
+    return keys.map(key => {
+        const {title, descr} = data[key];
+        return (
+            <AccordionItem key={key} value={key}>
+                <AccordionTrigger>{title}</AccordionTrigger>
+                <AccordionContent>
+                    <p>{descr}</p>
+                </AccordionContent>
+            </AccordionItem>
+        );
+    });
+};
 
 const PortfolioPage = () => {
+    const [portfolio, setPortfolio] = useState<Record<string, any> | null>(null);
     const cardYSpacing = 8;
     const email = process.env.NEXT_PUBLIC_APP_EMAIL || 'email-not-set@e.com';
 
+    useEffect(() => {
+        // Use the fetch API to get data from your API route
+        fetch('/api/portfolio')
+            .then(response => response.json())
+            .then(data => setPortfolio(data))
+            .catch(error => console.log('Fetching portfolio data failed:', error));
+    }, []);
+
+
+    if (!portfolio) {
+        return <LoadingSkeleton loadingText="Loading data from the database..."/>;
+    }
+
     return (
-        <div className="p-6">
-            <Card className="p-3">
-                <CardNextUI>
-                    <CardHeader>
-                        <h2>Skills</h2>
-                    </CardHeader>
-                    <Divider/>
-                    <CardBody>
-                        <section id="skills">
-                            <Accordion type="single" collapsible>
-                                <AccordionItem value="computer-skills">
-                                    <AccordionTrigger>Computer Skills</AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>Programming (C++, C, Python, Flutter, Java, MATLAB, HTML, JavaScript, PHP,
-                                            Qt, VHDL),
-                                            Linux, problem solving, image and text processing programs, LaTeX etc.</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                            </Accordion>
-                        </section>
-                    </CardBody>
-                </CardNextUI>
+        <FadeIn>
+            <div className="p-6">
+                <Card className="p-3">
+                    <CardNextUI>
+                        <CardHeaderNextUI>
+                            <h2>Skills</h2>
+                        </CardHeaderNextUI>
+                        <Divider/>
+                        <CardBody>
+                            <section id="skills">
+                                <Accordion type="single" collapsible>
+                                    {renderAccordionItems(portfolio.skills, Object.keys(portfolio.skills))}
+                                </Accordion>
+                            </section>
+                        </CardBody>
+                    </CardNextUI>
 
-                <Spacer y={cardYSpacing}/>
-                <CardNextUI>
-                    <CardHeader>
-                        <h2>Experience</h2>
-                    </CardHeader>
-                    <Divider/>
-                    <CardBody>
-                        <section id="experience">
-                            <Accordion type="single" collapsible>
-                                <AccordionItem value="nordanstigskustens">
-                                    <AccordionTrigger>Nordanstigskustens församling / Garden worker
-                                    </AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>Summer 2021,2022,2023 - Harmånger, Sweden</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                                <AccordionItem value="cdrsp-politecnico">
-                                    <AccordionTrigger>CDRSP Politécnico de Leiria / Internship in design and product
-                                        development
-                                        (APL)</AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>November 2019 – December 2019, Leiria, Portugal</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                            </Accordion>
-                        </section>
-                    </CardBody>
-                </CardNextUI>
+                    <Spacer y={cardYSpacing}/>
+                    <CardNextUI>
+                        <CardHeaderNextUI>
+                            <h2>Experience</h2>
+                        </CardHeaderNextUI>
+                        <Divider/>
+                        <CardBody>
+                            <section id="experience">
+                                <Accordion type="single" collapsible>
+                                    {renderAccordionItems(portfolio.experience, Object.keys(portfolio.experience))}
+                                </Accordion>
+                            </section>
+                        </CardBody>
+                    </CardNextUI>
 
-                <Spacer y={cardYSpacing}/>
-                <CardNextUI>
-                    <CardHeader>
-                        <h2>Education</h2>
-                    </CardHeader>
-                    <Divider/>
-                    <CardBody>
-                        <section id="education">
-                            <Accordion type="single" collapsible>
-                                <AccordionItem value="mid-sweden-university">
-                                    <AccordionTrigger>Mid Sweden University / Civil Engineer in Computer
-                                        Science</AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>August 2021 - Ongoing, Sundsvall, Sweden</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                                <AccordionItem value="bromangymnasiet">
-                                    <AccordionTrigger>Bromangymnasiet / Technology Programme</AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>August 2016 - June 2019, Hudiksvall, Sweden</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                                <AccordionItem value="elementary-school">
-                                    <AccordionTrigger>Elementary School</AccordionTrigger>
-                                    <AccordionContent>
-                                        <p>September 2005 - June 2014, Albania</p>
-                                    </AccordionContent>
-                                </AccordionItem>
-                            </Accordion>
-                        </section>
-                    </CardBody>
-                </CardNextUI>
+                    <Spacer y={cardYSpacing}/>
+                    <CardNextUI>
+                        <CardHeaderNextUI>
+                            <h2>Education</h2>
+                        </CardHeaderNextUI>
+                        <Divider/>
+                        <CardBody>
+                            <section id="education">
+                                <Accordion type="single" collapsible>
+                                    {renderAccordionItems(portfolio.education, Object.keys(portfolio.education))}
+                                </Accordion>
+                            </section>
+                        </CardBody>
+                    </CardNextUI>
 
-                <Spacer y={cardYSpacing}/>
-                <CardNextUI>
-                    <CardHeader>
-                        <h2>Languages</h2>
-                    </CardHeader>
-                    <Divider/>
-                    <CardBody>
-                        <section id="languages">
-                            <Badge color="primary">Swedish - Fluent</Badge>
-                            <Badge color="primary">English - Fluent</Badge>
-                            <Badge color="primary">Italian - Fluent</Badge>
-                            <Badge color="primary">Spanish - Beginner</Badge>
-                            <Badge color="primary">Albanian - Native</Badge>
-                        </section>
-                    </CardBody>
-                </CardNextUI>
+                    <Spacer y={cardYSpacing}/>
+                    <CardNextUI>
+                        <CardHeaderNextUI>
+                            <h2>Languages</h2>
+                        </CardHeaderNextUI>
+                        <Divider/>
+                        <CardBody>
+                            <section id="languages">
+                                {
+                                    Object.entries(portfolio.languages).map(([key, value]) => (
+                                        <Badge key={key} color="primary">{String(value)}</Badge>
+                                    ))
+                                }
+                            </section>
+                        </CardBody>
+                    </CardNextUI>
 
-                <Spacer y={cardYSpacing}/>
-                <CardNextUI>
-                    <CardHeader>
-                        <h2>Contact Me</h2>
-                    </CardHeader>
-                    <Divider/>
-                    <CardBody>
-                        <section id="contact">
-                            <CardNextUI className="w-fit p-2">
-                                <h3 className="p-2">Email</h3>
-                                <div className="flex ">
-                                    <CopyEmailButton email={email}/>
-                                    <Spacer x={2}/>
-                                    <SendEmailButton email={email}/>
-                                </div>
-                            </CardNextUI>
-                        </section>
-                    </CardBody>
-                </CardNextUI>
-            </Card>
-        </div>
+                    <Spacer y={cardYSpacing}/>
+                    <CardNextUI>
+                        <CardHeaderNextUI>
+                            <h2>Contact Me</h2>
+                        </CardHeaderNextUI>
+                        <Divider/>
+                        <CardBody>
+                            <section id="contact">
+                                <CardNextUI className="w-fit p-2">
+                                    <h3 className="p-2">Email</h3>
+                                    <div className="flex ">
+                                        <CopyEmailButton email={email}/>
+                                        <Spacer x={2}/>
+                                        <SendEmailButton email={email}/>
+                                    </div>
+                                </CardNextUI>
+                            </section>
+                        </CardBody>
+                    </CardNextUI>
+                </Card>
+            </div>
+        </FadeIn>
     );
 };
 
