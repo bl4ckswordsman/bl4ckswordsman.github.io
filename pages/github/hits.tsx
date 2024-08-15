dfc48d10b931ac51fa054aca572e0aa1bacbbb27
amarildo <amarildo.rajta@gmail.com>
2024-08-15 16:33:15 +0200

    add new repo edu-vault to daily hits page

--- pages/github/hits.tsx
+++ pages/github/hits.tsx
@@ -15,6 +15,7 @@ const repos = [
     'bl4ckswordsman/DT096G',
     'bl4ckswordsman/bl4ckswordsman',
     'bl4ckswordsman/ug-thesis-project',
+    'bl4ckswordsman/edu-vault',
     // More repository links that have the daily-hits badge to be added here
 ];
 
