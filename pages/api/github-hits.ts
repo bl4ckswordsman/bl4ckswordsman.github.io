cc19614a3ce5e7bc1a4328fb7023a4019dcf7f73
amarildo <amarildo.rajta@gmail.com>
2024-09-07 16:32:07 +0200

    temporarily add some debug prints for troubleshooting

--- pages/api/github-hits.ts
+++ pages/api/github-hits.ts
@@ -1,30 +1,30 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-import {parseSVG} from '@/lib/parse-svg';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { parseSVG } from '@/lib/parse-svg';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const {repo} = req.query;
+    const { repo } = req.query;
 
     if (!repo || typeof repo !== 'string') {
-        return res.status(400).json({error: 'Repository parameter is required'});
+        return res.status(400).json({ error: 'Repository parameter is required' });
     }
 
     try {
         const url = `https://hits.seeyoufarm.com/api/count/graph/dailyhits.svg?url=https://github.com/${repo}`;
-        // console.log(`Fetching data from: ${url}`);
+        console.log(`Fetching data from: ${url}`);
 
         const response = await fetch(url);
         if (!response.ok) {
-            console.error(`Failed to fetch data from ${url}`);
-            return res.status(500).json({error: `Failed to fetch data from ${url}`});
+            const errorText = await response.text();
+            console.error(`Failed to fetch data from ${url}: ${errorText}`);
+            return res.status(500).json({ error: `Failed to fetch data from ${url}: ${errorText}` });
         }
 
         const svgText = await response.text();
-
         const graphData = await parseSVG(svgText);
 
         if (!graphData.dates.length || !graphData.counts.length) {
             console.error('Parsed data is incomplete or invalid.');
-            return res.status(500).json({error: 'Unable to parse SVG data properly'});
+            return res.status(500).json({ error: 'Unable to parse SVG data properly' });
         }
 
         const parsedData = graphData.dates.map((date, index) => ({
@@ -34,7 +34,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         res.status(200).json(parsedData);
     } catch (error) {
-        console.error('Error fetching or processing SVG data for' + repo + ':', error);
-        res.status(500).json({error: 'Failed to fetch or process SVG data for ' + repo});
+        console.error('Error fetching or processing SVG data for ' + repo + ':', error);
+        res.status(500).json({ error: 'Failed to fetch or process SVG data for ' + repo });
     }
 }
